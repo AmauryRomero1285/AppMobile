4d8df2582907cad627d1298315c563fc08cf9dc6
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2025-01-14 17:14:28 -0600

    Class2
    Adición de la primera ruta al servidor

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //Aquí va la configuración del Servidor
-import express from 'express';
+import express from 'express';//sirve para crear
 import morgan from 'morgan';
 import studentsRoutes from './routes/students.routes.js';
 const app=express();
@@ -13,6 +13,6 @@ app.use(express.urlencoded({extended:true}));//entiende formularios
 app.use(morgan('dev'));//vizualizar las peticiones en consola (saber si llegó o no la petición)**se activa aquí**
 
 //Routes
-app.use("/api/students", studentsRoutes);//envia a una ruta para activar el controlador(crea la ruta)[inoca una función]
+app.use("/api/students", studentsRoutes);//envia a una ruta para activar el controlador(crea la ruta)[invoca una función]**Es un apuntador**
 
 export default app;
