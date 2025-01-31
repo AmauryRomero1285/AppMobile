4064fa9f042301483923bb329a8802cdf2cb0d26
AmauryRomero1285 <230190@utxicotepec.edu.mx>
2025-01-31 17:26:42 -0600

    class 07
    ejs

--- src/controllers/students.controller.js
+++ src/controllers/students.controller.js
@@ -6,13 +6,13 @@ const studentsController = {};
 studentsController.getAll = (req, res) => {
   studentDAO
     .getAll()
-    .then((students) => {
-      console.log(students);
+    .then((student) => {
+      console.log(student);
      /* res.json({
         //Aqui se le pide a DAO los datos de los estudiantes
         data: students,
       });*/
-       res.render('../src/views/index.ejs');//guia la renderización a la carpeta
+       res.render('../src/views/index.ejs',{student});//guia la renderización a la carpeta
     })
     .catch((error) => {
       res.json({
@@ -24,17 +24,18 @@ studentsController.getAll = (req, res) => {
 };
 
 studentsController.getOne = (req, res) => {
-  studentDAO.getOne(req.params.student_id).then((students) => {
-    if (students != null) res.json({ data: students });
-    else res.json({ message: "Student not found" });
+  studentDAO.getOne(req.params.student_id).then((student) => {
+   /* if (student != null) res.json({ data: student });
+    else res.json({ message: "Student not found" });*/
+    res.render('../src/views/edit.ejs',{student});
   });
 };
 
 studentsController.insert = (req, res) => {
   studentDAO
     .insert(req.body)
-    .then((response) => {
-      res.json({
+    .then((student) => {
+      /*res.json({
         data: {
           message: "Student saved",
           student: response,
@@ -42,20 +43,22 @@ studentsController.insert = (req, res) => {
       });
     })
     .catch((error) => {
-      res.json({ data: { message: error } });
+      res.json({ data: { message: error } });*/
+      res.redirect('/api/students/getAll')//redirigimos al getAll
     });
 };
 
 studentsController.updateOne = (req, res) => {
   studentDAO
     .updateOne(req.params.student_id, req.body)
-    .then((response) => {
-      res.json({
+    .then((student) => {
+      /*res.json({
         data: {
           message: "Student updated successfully",
           student: response,
         },
-      });
+      });*/
+      res.redirect('api/students/getAll');
     })
     .catch((error) => {
       res.json({ data: { message: error } });
@@ -65,12 +68,13 @@ studentsController.deleteOne = (req, res) => {
   studentDAO
     .deleteOne(req.params.student_id)
     .then((response) => {
-      res.json({
+      /*res.json({
         data: {
           message: "Student deleted successfully",
           student: response,
         },
-      });
+      });*/
+      res.redirect('/api/students/getAll')
     })
     .catch((error) => {
       data: {
